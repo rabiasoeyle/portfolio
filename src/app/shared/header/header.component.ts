e81c4af54730a8296ff277125098f5b440e83611
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2024-09-25 09:39:57 +0200

    toggle menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  menuIsOpen:boolean = false;
+  constructor(){
+
+  }
+  toggleMenu(){
+    this.menuIsOpen =!this.menuIsOpen;
+  }
+  
+  //This is a eventlistener for the situation, that the toggle-content is open and the user clicks 
+  //somewhere else then the toggle button or content.
+  @HostListener('document:click', ['$event'])
+  handleClick(event: Event) {
+    const toggleMenuButton = document.getElementById('toggleMenu');
+    const toggleContent = document.getElementById('toggleContent');
+    if (
+      this.menuIsOpen && 
+      toggleMenuButton && !toggleMenuButton.contains(event.target as Node) && 
+      toggleContent && !toggleContent.contains(event.target as Node)
+    ) {
+      this.menuIsOpen = false;
+    }
+  }
 
 }
