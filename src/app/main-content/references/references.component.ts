03b917feea4d45378f239a15209bde85ee41ac21
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2024-10-02 15:17:54 +0200

    animation my references  added

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, inject, ViewChildren, ElementRef, QueryList } from '@angular/core';
 import { ReferencesInterface } from '../../references';
 import { ReferencesService } from '../../references.service';
 
@@ -9,6 +9,48 @@ import { ReferencesService } from '../../references.service';
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss'
 })
-export class ReferencesComponent {
+export class ReferencesComponent implements AfterViewInit{
+  @ViewChildren('reference', { read: ElementRef }) examples!: QueryList<ElementRef>;
+  
+  ngAfterViewInit() {
+    this.examples.forEach((example, index) => {
+      console.log('Element:', example.nativeElement, 'Index:', index);
+      if(index == 0){
+        const elementRight = example.nativeElement;
+        const options = {
+          root: null,
+          rootMargin: '100px',
+          threshold: 0.3,
+        };
+          const observer = new IntersectionObserver((entries) => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                  elementRight.classList.add('showLeft');
+                  observer.unobserve(elementRight)
+            } 
+          });
+        }, options);
+        observer.observe(elementRight);
+      }else{}
+      if (index == 2){
+        const element = example.nativeElement;
+        const options = {
+        root: null,
+        rootMargin: '100px',
+        threshold: 0.3,
+        };
+        const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+                element.classList.add('show');
+                observer.unobserve(element)
+          } 
+        });
+    }, options);
+    observer.observe(element);
+  }else{
+  }
+});
+  }
 referenceList = inject(ReferencesService);
 }
