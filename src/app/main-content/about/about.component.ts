3cddd23937341db87e9c4c66925ad039a0b67f1e
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2024-10-02 13:40:41 +0200

    animation at about added

--- src/app/main-content/about/about.component.ts
+++ src/app/main-content/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +7,43 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit{
+  @ViewChild('img') myImg!: ElementRef;
+  @ViewChild('text') myText!: ElementRef;
 
+  ngAfterViewInit() {
+    let img = this.myImg.nativeElement;
+    let text = this.myText.nativeElement
+  
+    const options = {
+      root: null,
+      //null um als Ansichtsquelle den gesamten Browser zu nutzen
+      rootMargin: '100px',
+      //dient zum vergrößern oder verkleinern des Hauptrahmens des Wurzelelements
+      threshold: 0.2,
+      //gibt an wie viel Prozent des elements bereits sichtbar sein müssen, damit die animation ausgeführt wird
+      
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      //Der an den Konstruktor übergebene Rückruf IntersectionObserver()erhält 
+      //eine Liste von IntersectionObserverEntryObjekten und den Beobachter:
+      entries.forEach(entry => {
+        if (entry.isIntersecting && !text.classList.contains('showLeft')) {
+          img.classList.add('show');
+          text.classList.add('showLeft');
+          //beobachtung stoppen, damit es nur einmal animiert
+          observer.unobserve(img);
+          observer.unobserve(text);
+        } 
+        // else {
+        //   img.classList.remove('show');
+        //   text.classList.remove('showLeft');
+        // }
+      });
+    }, options);
+
+    observer.observe(img);
+    observer.observe(text);
+  }
 }
--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
-export class SkillsComponent {
-
+export class SkillsComponent implements AfterViewInit {
+  @ViewChild('lineOne') right!: ElementRef;
+  @ViewChild('ineTwo') left!: ElementRef;
+  
+  ngAfterViewInit() {
+    let right = this.right.nativeElement;
+    let left = this.left.nativeElement
+    const options = {
+      root: null,
+      rootMargin: '100px',
+      threshold: 0.2,
+    };
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting && !left.classList.contains('showLeft')) {
+          right.classList.add('show');
+          left.classList.add('showLeft');
+          observer.unobserve(right);
+          observer.unobserve(left);
+        } 
+      });
+    }, options);
+    observer.observe(right);
+    observer.observe(left);
+  }
 }
